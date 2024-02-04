8407ce6cea3992d89e31942a35a0df9139a75afa
RealFaceCode <kevin-ernst@xyz.de>
2024-02-04 12:17:14 +0100

    Update submodul

--- libBuild/hdr/netwrapper.hpp
+++ libBuild/hdr/netwrapper.hpp
@@ -1,7 +1,5 @@
 #pragma once
 
-
-
 #ifdef _WIN32
     #include <winsock2.h>
     #include <ws2tcpip.h>
@@ -17,23 +15,34 @@
 constexpr int SOCK_ERR = -1;
 constexpr int INVAL_SOCK = (~0);
 constexpr unsigned int MAX_CONN = 0x7fffffff;
+constexpr unsigned int MAX_BUF = 512;
 
-namespace eNet
+namespace enet
 {
-    int initNetwork();
-    int getAddressInfo(const char *hostname, const char *service, const addrinfo *hints, addrinfo **res);
-    socket_t createSocket(int domain, int type, int protocol);
-    socket_t createSocket(addrinfo* res);
-    int connectToServer(socket_t sockfd, const sockaddr *serv_addr, socklen_t addrlen);
-    void freeAddressInfo(addrinfo* res);
-    void closeSocket(socket_t sockfd);
+    int Init();
+    void Cleanup();
 
-    addrinfo fillAddrInfo(int family, int socktype, int protocol, int flags = 0, int addrlen = 0);
-    addrinfo * getAddrInfo(const char *hostname, const char *service, int family, int socktype, int protocol, int flags = 0, int addrlen = 0);
-    int connectToServer(socket_t sockfd, const addrinfo *res);
+    namespace internal
+    {
+        int GetAddressInfo(const char *hostname, const char *service, const addrinfo *hints, addrinfo **res);
+        socket_t CreateSocket(int domain, int type, int protocol);
+        socket_t CreateSocket(addrinfo* res);
+        int ConnectToServer(socket_t sockfd, const sockaddr *serv_addr, socklen_t addrlen);
+        void FreeAddressInfo(addrinfo* res);
+        void CloseSocket(socket_t sockfd);
 
-    int bind(socket_t sockfd, const addrinfo *res);
-    int listen(socket_t sockfd, int backlog = MAX_CONN);
+        addrinfo FillAddrInfo(int family, int socktype, int protocol, int flags = 0, int addrlen = 0);
+        addrinfo* GetAddressInfo(const char *hostname, const char *service, int family, int socktype, int protocol, int flags = 0, int addrlen = 0);
+        int ConnectToServer(socket_t sockfd, const addrinfo *res);
 
-    socket_t accept(socket_t sockfd, char** addrStr);
+        int Bind(socket_t sockfd, const addrinfo *res);
+        int Listen(socket_t sockfd, int backlog = MAX_CONN);
+
+        socket_t Accept(socket_t sockfd, char** addrStr);
+
+        int Send(socket_t sockfd, const char *buf, int len, int flags);
+        int Recv(socket_t sockfd, char *buf, int len, int flags);
+        int SendTo(socket_t sockfd, const char *buf, int len, int flags, const addrinfo* addr);
+        int RecvFrom(socket_t sockfd, char *buf, int len, int flags, addrinfo* addr);
+    }
 }
--- libBuild/src/netwrapper.cpp
+++ libBuild/src/netwrapper.cpp
@@ -2,9 +2,9 @@
 
 #include <cstring>
 
-namespace eNet
+namespace enet
 {
-    int initNetwork()
+    int Init()
     {
 #ifdef _WIN32
         WSADATA wsaData;
@@ -14,100 +14,134 @@ namespace eNet
 #endif
     }
 
-    int getAddressInfo(const char *hostname, const char *service, const addrinfo *hints, addrinfo **res)
+    void Cleanup()
     {
 #ifdef _WIN32
-        return ::getaddrinfo(hostname, service, (const addrinfo*)hints, res);
-#else
-        return ::getaddrinfo(hostname, service, hints, res);
+        ::WSACleanup();
 #endif
     }
 
-    socket_t createSocket(int domain, int type, int protocol)
+    namespace internal
     {
-        socket_t sock;
-        sock = ::socket(domain, type, protocol);
-        if (sock == SOCK_ERR) {
-            return SOCK_ERR;
-        }
-        return sock;
-    }
-
-    socket_t createSocket(addrinfo* res)
-    {
-        return createSocket(res->ai_family, res->ai_socktype, res->ai_protocol);
-    }
-
-    int connectToServer(socket_t sockfd, const sockaddr *serv_addr, socklen_t addrlen)
-    {
+        int GetAddressInfo(const char *hostname, const char *service, const addrinfo *hints, addrinfo **res)
+        {
 #ifdef _WIN32
-        return ::connect(sockfd, serv_addr, (int)addrlen);
+            return ::getaddrinfo(hostname, service, (const addrinfo*)hints, res);
 #else
-        return ::connect(sockfd, serv_addr, addrlen);
+            return ::getaddrinfo(hostname, service, hints, res);
 #endif
-    }
-
-    void freeAddressInfo(addrinfo *res)
-    {
-        ::freeaddrinfo(res);
-    }
-
-    void closeSocket(socket_t sockfd)
-    {
-#ifdef _WIN32
-        ::closesocket(sockfd);
-#else
-        ::close(sockfd);
-#endif
-    }
-
-    addrinfo fillAddrInfo(int family, int socktype, int protocol, int flags, int addrlen)
-    {
-        addrinfo hints;
-        std::memset(&hints, 0, sizeof(hints));
-        hints.ai_family = family;
-        hints.ai_socktype = socktype;
-        hints.ai_protocol = protocol;
-        hints.ai_flags = flags;
-        hints.ai_addrlen = addrlen;
-        return hints;
-    }
-
-    addrinfo * getAddrInfo(const char *hostname, const char *service, int family, int socktype, int protocol, int flags, int addrlen)
-    {
-        addrinfo hints = fillAddrInfo(family, socktype, protocol, flags, addrlen);
-        addrinfo *result;
-
-        getAddressInfo(hostname, service, &hints, &result);
-
-        return result;
-    }
-
-    int connectToServer(socket_t sockfd, const addrinfo *res)
-    {
-        return connectToServer(sockfd, (const sockaddr*)res->ai_addr, (socklen_t)res->ai_addrlen);
-    }
-
-    int bind(socket_t sockfd, const addrinfo *res)
-    {
-        return ::bind(sockfd, res->ai_addr, res->ai_addrlen);
-    }
-
-    int listen(socket_t sockfd, int backlog)
-    {
-        return ::listen(sockfd, backlog);
-    }
-
-    socket_t accept(socket_t sockfd, char** addrStr)
-    {
-        sockaddr_storage their_addr;
-        socklen_t sin_size = sizeof their_addr;
-        socket_t sock = ::accept(sockfd, (sockaddr *)&their_addr, &sin_size);
-        if (sock == SOCK_ERR) {
-            return SOCK_ERR;
         }
 
-        *addrStr = inet_ntoa(((sockaddr_in *)&their_addr)->sin_addr);
-        return sock;
+        socket_t CreateSocket(int domain, int type, int protocol)
+        {
+            socket_t sock;
+            sock = ::socket(domain, type, protocol);
+            if (sock == SOCK_ERR) {
+                return SOCK_ERR;
+            }
+            return sock;
+        }
+
+        socket_t CreateSocket(addrinfo* res)
+        {
+            return CreateSocket(res->ai_family, res->ai_socktype, res->ai_protocol);
+        }
+
+        int ConnectToServer(socket_t sockfd, const sockaddr *serv_addr, socklen_t addrlen)
+        {
+#ifdef _WIN32
+            return ::connect(sockfd, serv_addr, (int)addrlen);
+#else
+            return ::connect(sockfd, serv_addr, addrlen);
+#endif
+        }
+
+        void FreeAddressInfo(addrinfo *res)
+        {
+            ::freeaddrinfo(res);
+        }
+
+        void CloseSocket(socket_t sockfd)
+        {
+#ifdef _WIN32
+            ::closesocket(sockfd);
+#else
+            ::close(sockfd);
+#endif
+        }
+
+        addrinfo FillAddrInfo(int family, int socktype, int protocol, int flags, int addrlen)
+        {
+            addrinfo hints;
+            std::memset(&hints, 0, sizeof(hints));
+            hints.ai_family = family;
+            hints.ai_socktype = socktype;
+            hints.ai_protocol = protocol;
+            hints.ai_flags = flags;
+            hints.ai_addrlen = addrlen;
+            return hints;
+        }
+
+        addrinfo * GetAddressInfo(const char *hostname, const char *service, int family, int socktype, int protocol, int flags, int addrlen)
+        {
+            addrinfo hints = FillAddrInfo(family, socktype, protocol, flags, addrlen);
+            addrinfo *result;
+
+            GetAddressInfo(hostname, service, &hints, &result);
+
+            return result;
+        }
+
+        int ConnectToServer(socket_t sockfd, const addrinfo *res)
+        {
+            return ConnectToServer(sockfd, (const sockaddr*)res->ai_addr, (socklen_t)res->ai_addrlen);
+        }
+
+        int Bind(socket_t sockfd, const addrinfo *res)
+        {
+            return ::bind(sockfd, res->ai_addr, res->ai_addrlen);
+        }
+
+        int Listen(socket_t sockfd, int backlog)
+        {
+            return ::listen(sockfd, backlog);
+        }
+
+        socket_t Accept(socket_t sockfd, char** addrStr)
+        {
+            sockaddr_storage their_addr;
+            socklen_t sin_size = sizeof their_addr;
+            socket_t sock = ::accept(sockfd, (sockaddr *)&their_addr, &sin_size);
+            if (sock == SOCK_ERR) {
+                return SOCK_ERR;
+            }
+
+            *addrStr = ::inet_ntoa(((sockaddr_in *)&their_addr)->sin_addr);
+            return sock;
+        }
+
+        int Send(socket_t sockfd, const char *buf, int len, int flags)
+        {
+            return ::send(sockfd, buf, len, flags);
+        }
+
+        int Recv(socket_t sockfd, char *buf, int len, int flags)
+        {
+            return ::recv(sockfd, buf, len, flags);
+        }
+
+        int SendTo(socket_t sockfd, const char *buf, int len, int flags, const addrinfo* addr)
+        {
+            return ::sendto(sockfd, buf, len, flags, addr->ai_addr, addr->ai_addrlen);
+        }
+
+        int RecvFrom(socket_t sockfd, char *buf, int len, int flags, addrinfo* addr )
+        {
+#ifdef _WIN32
+            return ::recvfrom(sockfd, buf, len, flags, addr->ai_addr, (socklen_t*)addr->ai_addrlen);
+#else
+            return ::recvfrom(sockfd, buf, len, flags, addr->ai_addr, addr->ai_addrlen);
+#endif
+        }
     }
 }
