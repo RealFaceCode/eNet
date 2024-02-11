a96e5b7a249d164df240a5245eda86af089b43c5
RealFaceCode <kevin-ernst@xyz.de>
2024-02-11 15:50:49 +0100

    Add new error codes for network operations

--- libBuild/hdr/err.hpp
+++ libBuild/hdr/err.hpp
@@ -15,6 +15,11 @@ namespace enet::enums
         RECV_HEADER_FAILED = 8,
         RECV_ORDER_FAILED = 9,
         RECV_PACKAGES_FAILED = 10,
+        BIND_FAILED = 11,
+        LISTEN_FAILED = 12,
+        ACCEPT_FAILED = 13,
+        CONNECT_FAILED = 14,
+        CLOSE_FAILED = 15,
     };
 
     bool operator! (Err err);
